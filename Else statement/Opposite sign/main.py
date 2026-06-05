c37cd990f9b3be0405bc65f2e8b4df3ff4a94ff2
Nino <samuelnunessaldanha2007@gmail.com>
2026-06-05 20:51:34 -0300

    Else

---
+++ Else statement/Opposite sign/main.py
@@ -0,0 +1,9 @@
+# put your code here
+number = int(input())
+number *= -1
+
+print(number)
+if number > 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
---
+++ Else statement/Print positive value or negated negative/main.py
@@ -0,0 +1,6 @@
+num = int(input())
+
+if num > 0:
+    print(num)
+else:
+    print(num * -1)
