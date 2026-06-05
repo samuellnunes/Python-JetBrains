9e84c7fc5202c9aa271a9d1bd61fc2d9fad4961a
Nino <samuelnunessaldanha2007@gmail.com>
2026-06-04 23:56:45 -0300

    Comparing

---
+++ Comparisons/Comparisons/main.py
@@ -0,0 +1,4 @@
+#  You can experiment here, it won't be checked
+dividend = int(input())
+divisor = int(input())
+print(dividend % divisor == 0)
---
+++ Comparisons/Movie theater/main.py
@@ -0,0 +1,5 @@
+numberHalls = int(input())
+capacityHalls = int(input())
+quantityViwers = int(input())
+
+print(numberHalls * capacityHalls >= quantityViwers)
